66b6b61acbf1854976490e9430fee6de12b136fb
dnjstjr93 <dnjstjr93@gmail.com>
2023-07-24 13:41:52 +0900

    Initial commit

---
+++ tas_sample/tas_Drone/tas_SITL.js
@@ -0,0 +1,456 @@
+/**
+ * Created by Wonseok Jung on 2023-07-24.
+ */
+
+const mqtt = require('mqtt');
+const {nanoid} = require('nanoid');
+const dgram = require("dgram");
+
+const mavlink = require('./mavlibrary/mavlink');
+
+/* USER CODE */
+// for sensor
+let tas = {
+    client: {
+        connected: false,
+    },
+
+    connection: {
+        host: '127.0.0.1',
+        port: 1883,
+        endpoint: '',
+        clean: true,
+        reconnectPeriod: 2000,
+        connectTimeout: 30000,
+        queueQoSZero: false,
+        clientId: 'tas_' + nanoid(15),
+        username: 'keti_thyme',
+        password: 'keti_thyme',
+    },
+};
+
+let PORT1 = 14555; // output: SITL --> GCS
+let PORT2 = 14556; // input : GCS --> SITL
+
+let sitlUDP1 = null;
+let sitlUDP2 = dgram.createSocket('udp4'); // to send commands received from gcs
+
+
+let sendDataTopic = {
+    drone: '/thyme/drone',
+};
+
+let recvDataTopic = {
+    gcs: '/gcs/cmd',
+};
+/* */
+
+let createConnection = () => {
+    if (tas.client.connected) {
+        console.log('Already connected --> destroyConnection')
+        destroyConnection();
+    }
+
+    if (!tas.client.connected) {
+        tas.client.loading = true;
+        const {host, port, endpoint, ...options} = tas.connection;
+        const connectUrl = `mqtt://${host}:${port}${endpoint}`
+        try {
+            tas.client = mqtt.connect(connectUrl, options);
+
+            tas.client.on('connect', () => {
+                console.log(host, 'Connection succeeded!');
+
+                tas.client.connected = true;
+                tas.client.loading = false;
+
+                for (let topicName in recvDataTopic) {
+                    if (recvDataTopic.hasOwnProperty(topicName)) {
+                        doSubscribe(recvDataTopic[topicName]);
+                    }
+                }
+
+                SitlUdpOpening();
+            });
+
+            tas.client.on('error', (error) => {
+                console.log('Connection failed', error);
+
+                destroyConnection();
+            });
+
+            tas.client.on('close', () => {
+                console.log('Connection closed');
+
+                destroyConnection();
+            });
+
+            tas.client.on('message', (topic, message) => {
+                /* USER CODES */
+                if (topic === recvDataTopic.gcs) {
+                    if (sitlUDP2 !== null) {
+                        sitlUDP2.send(message, 0, message.length, PORT2, tas.connection.host,
+                            function (err) {
+                                if (err) {
+                                    console.log('UDP message send error', err);
+                                    return;
+                                }
+                            });
+                    }
+                }
+                /* */
+            });
+        } catch (error) {
+            console.log('mqtt.connect error', error);
+            tas.client.connected = false;
+        }
+    }
+};
+
+let doSubscribe = (topic) => {
+    if (tas.client.connected) {
+        const qos = 0;
+        tas.client.subscribe(topic, {qos}, (error) => {
+            if (error) {
+                console.log('Subscribe to topics error', error)
+                return;
+            }
+
+            console.log('Subscribe to topics (', topic, ')');
+        });
+    }
+};
+
+let doUnSubscribe = (topic) => {
+    if (tas.client.connected) {
+        tas.client.unsubscribe(topic, error => {
+            if (error) {
+                console.log('Unsubscribe error', error)
+            }
+
+            console.log('Unsubscribe to topics (', topic, ')');
+        });
+    }
+};
+
+let doPublish = (topic, payload) => {
+    if (tas.client.connected) {
+        tas.client.publish(topic, payload, 0, error => {
+            if (error) {
+                console.log('Publish error', error)
+            }
+        });
+    }
+};
+
+let destroyConnection = () => {
+    if (tas.client.connected) {
+        try {
+            if (Object.hasOwnProperty.call(tas.client, '__ob__')) {
+                tas.client.end();
+            }
+            tas.client = {
+                connected: false,
+                loading: false
+            }
+            console.log(this.name, 'Successfully disconnected!');
+        } catch (error) {
+            console.log('Disconnect failed', error.toString())
+        }
+    }
+};
+
+createConnection();
+
+/* USER CODE */
+function SitlUdpOpening() {
+    if (sitlUDP1 === null) {
+        sitlUDP1 = dgram.createSocket('udp4');
+        sitlUDP1.bind(PORT1, tas.connection.host);
+
+        sitlUDP1.on('listening', SitlUdpOpen);
+        sitlUDP1.on('message', SitlUdpData);
+        sitlUDP1.on('close', SitlUdpClose);
+        sitlUDP1.on('error', SitlUdpError);
+    }
+}
+
+function SitlUdpOpen() {
+    console.log('UDP socket connect to ' + sitlUDP1.address().address + ':' + sitlUDP1.address().port);
+}
+
+function SitlUdpClose() {
+    console.log('sitlUDP1 closed.');
+
+    setTimeout(SitlUdpOpening, 2000);
+}
+
+function SitlUdpError(error) {
+    console.log('sitlUDP1 error: ' + error.message);
+
+    setTimeout(SitlUdpOpening, 2000);
+}
+
+var mavStrFromDrone = '';
+var mavStrFromDroneLength = 0;
+var mavVersion = 'unknown';
+var mavVersionCheckFlag = false;
+
+function SitlUdpData(data) {
+    mavStrFromDrone += data.toString('hex').toLowerCase();
+
+    while (mavStrFromDrone.length > 20) {
+        if (!mavVersionCheckFlag) {
+            var stx = mavStrFromDrone.substring(0, 2);
+            if (stx === 'fe') {
+                var len = parseInt(mavStrFromDrone.substring(2, 4), 16);
+                var mavLength = (6 * 2) + (len * 2) + (2 * 2);
+                var sysid = parseInt(mavStrFromDrone.substring(6, 8), 16);
+                var msgid = parseInt(mavStrFromDrone.substring(10, 12), 16);
+
+                if (msgid === 0 && len === 9) { // HEARTBEAT
+                    mavVersionCheckFlag = true;
+                    mavVersion = 'v1';
+                }
+
+                if (mavStrFromDrone.length >= mavLength) {
+                    var mavPacket = mavStrFromDrone.substring(0, mavLength);
+
+                    mavStrFromDrone = mavStrFromDrone.substring(mavLength);
+                    mavStrFromDroneLength = 0;
+                } else {
+                    break;
+                }
+            } else if (stx === 'fd') {
+                len = parseInt(mavStrFromDrone.substring(2, 4), 16);
+                mavLength = (10 * 2) + (len * 2) + (2 * 2);
+
+                sysid = parseInt(mavStrFromDrone.substring(10, 12), 16);
+                msgid = parseInt(mavStrFromDrone.substring(18, 20) + mavStrFromDrone.substring(16, 18) + mavStrFromDrone.substring(14, 16), 16);
+
+                if (msgid === 0 && len === 9) { // HEARTBEAT
+                    mavVersionCheckFlag = true;
+                    mavVersion = 'v2';
+                }
+                if (mavStrFromDrone.length >= mavLength) {
+                    mavPacket = mavStrFromDrone.substring(0, mavLength);
+
+                    mavStrFromDrone = mavStrFromDrone.substring(mavLength);
+                    mavStrFromDroneLength = 0;
+                } else {
+                    break;
+                }
+            } else {
+                mavStrFromDrone = mavStrFromDrone.substring(2);
+            }
+        } else {
+            stx = mavStrFromDrone.substring(0, 2);
+            if (mavVersion === 'v1' && stx === 'fe') {
+                len = parseInt(mavStrFromDrone.substring(2, 4), 16);
+                mavLength = (6 * 2) + (len * 2) + (2 * 2);
+
+                if (mavStrFromDrone.length >= mavLength) {
+                    mavPacket = mavStrFromDrone.substring(0, mavLength);
+                    console.log('v1', mavPacket);
+
+                    doPublish(sendDataTopic.drone, mavPacket);
+
+                    // setTimeout(parseMavFromDrone, 0, mavPacket);
+
+                    mavStrFromDrone = mavStrFromDrone.substring(mavLength);
+                    mavStrFromDroneLength = 0;
+                } else {
+                    break;
+                }
+            } else if (mavVersion === 'v2' && stx === 'fd') {
+                len = parseInt(mavStrFromDrone.substring(2, 4), 16);
+                mavLength = (10 * 2) + (len * 2) + (2 * 2);
+
+                if (mavStrFromDrone.length >= mavLength) {
+                    mavPacket = mavStrFromDrone.substring(0, mavLength);
+                    console.log('v2', mavPacket);
+
+                    doPublish(sendDataTopic.drone, mavPacket);
+
+                    // setTimeout(parseMavFromDrone, 0, mavPacket);
+
+                    mavStrFromDrone = mavStrFromDrone.substring(mavLength);
+                    mavStrFromDroneLength = 0;
+                } else {
+                    break;
+                }
+            } else {
+                mavStrFromDrone = mavStrFromDrone.substring(2);
+            }
+        }
+    }
+}
+
+var fc = {};
+var flag_base_mode = 0;
+
+function parseMavFromDrone(mavPacket) {
+    try {
+        var ver = mavPacket.substring(0, 2);
+        var sys_id = '';
+        var msg_id = '';
+        var base_offset = 12;
+
+        if (ver === 'fd') {
+            sys_id = parseInt(mavPacket.substring(10, 12).toLowerCase(), 16);
+            msg_id = parseInt(mavPacket.substring(18, 20) + mavPacket.substring(16, 18) + mavPacket.substring(14, 16), 16);
+            base_offset = 20;
+        } else {
+            sys_id = parseInt(mavPacket.substring(6, 8).toLowerCase(), 16);
+            msg_id = parseInt(mavPacket.substring(10, 12).toLowerCase(), 16);
+            base_offset = 12;
+        }
+        My_sys_id = sys_id;
+
+        if (msg_id === mavlink.MAVLINK_MSG_ID_HEARTBEAT) { // #00 : HEARTBEAT
+            var custom_mode = mavPacket.substring(base_offset, base_offset + 8).toLowerCase();
+            base_offset += 8;
+            var type = mavPacket.substring(base_offset, base_offset + 2).toLowerCase();
+            base_offset += 2;
+            var autopilot = mavPacket.substring(base_offset, base_offset + 2).toLowerCase();
+            base_offset += 2;
+            var base_mode = mavPacket.substring(base_offset, base_offset + 2).toLowerCase();
+            base_offset += 2;
+            var system_status = mavPacket.substring(base_offset, base_offset + 2).toLowerCase();
+            base_offset += 2;
+            var mavlink_version = mavPacket.substring(base_offset, base_offset + 2).toLowerCase();
+
+            fc.heartbeat = {};
+            fc.heartbeat.type = Buffer.from(type, 'hex').readUInt8(0);
+            if (fc.heartbeat.type !== mavlink.MAV_TYPE_ADSB) {
+                fc.heartbeat.autopilot = Buffer.from(autopilot, 'hex').readUInt8(0);
+                fc.heartbeat.base_mode = Buffer.from(base_mode, 'hex').readUInt8(0);
+                fc.heartbeat.custom_mode = Buffer.from(custom_mode, 'hex').readUInt32LE(0);
+                fc.heartbeat.system_status = Buffer.from(system_status, 'hex').readUInt8(0);
+                fc.heartbeat.mavlink_version = Buffer.from(mavlink_version, 'hex').readUInt8(0);
+
+                let armStatus = (fc.heartbeat.base_mode & 0x80) === 0x80;
+
+                if(my_sortie_name === 'unknown') {
+                    if (armStatus) {
+                        flag_base_mode++;
+                        if (flag_base_mode === 3) {
+                            my_sortie_name = 'arm';
+                            setSortieName('unknown-arm:' + fc.global_position_int.time_boot_ms.toString());
+                        }
+                    }
+                    else {
+                        flag_base_mode = 0;
+                        my_sortie_name = 'disarm';
+                        setSortieName('unknown-disarm:0');
+                    }
+                }
+                else if(my_sortie_name === 'disarm') {
+                    if (armStatus) {
+                        flag_base_mode++;
+                        if (flag_base_mode === 3) {
+                            my_sortie_name = 'arm';
+                            setSortieName('disarm-arm:' + fc.global_position_int.time_boot_ms.toString());
+                        }
+                    }
+                    else {
+                        flag_base_mode = 0;
+                        my_sortie_name = 'disarm';
+                    }
+                }
+                else if(my_sortie_name === 'arm') {
+                    if (armStatus) {
+                        my_sortie_name = 'arm';
+                    }
+                    else {
+                        flag_base_mode = 0;
+                        my_sortie_name = 'disarm';
+                        setSortieName('arm-disarm:0');
+                    }
+                }
+            }
+        } else if (msg_id === mavlink.MAVLINK_MSG_ID_GLOBAL_POSITION_INT) { // #33
+            var time_boot_ms = mavPacket.substring(base_offset, base_offset + 8).toLowerCase();
+            base_offset += 8;
+            var lat = mavPacket.substring(base_offset, base_offset + 8).toLowerCase();
+            base_offset += 8;
+            var lon = mavPacket.substring(base_offset, base_offset + 8).toLowerCase();
+            base_offset += 8;
+            var alt = mavPacket.substring(base_offset, base_offset + 8).toLowerCase();
+            base_offset += 8;
+            var relative_alt = mavPacket.substring(base_offset, base_offset + 8).toLowerCase();
+            base_offset += 8;
+            var vx = mavPacket.substring(base_offset, base_offset + 4).toLowerCase();
+            base_offset += 4;
+            var vy = mavPacket.substring(base_offset, base_offset + 4).toLowerCase();
+            base_offset += 4;
+            var vz = mavPacket.substring(base_offset, base_offset + 4).toLowerCase();
+            base_offset += 4;
+            var hdg = mavPacket.substring(base_offset, base_offset + 4).toLowerCase();
+
+            fc.global_position_int = {};
+            fc.global_position_int.time_boot_ms = Buffer.from(time_boot_ms, 'hex').readUInt32LE(0);
+            fc.global_position_int.lat = Buffer.from(lat, 'hex').readInt32LE(0);
+            fc.global_position_int.lon = Buffer.from(lon, 'hex').readInt32LE(0);
+            fc.global_position_int.alt = Buffer.from(alt, 'hex').readInt32LE(0);
+            fc.global_position_int.relative_alt = Buffer.from(relative_alt, 'hex').readInt32LE(0);
+            fc.global_position_int.vx = Buffer.from(vx, 'hex').readInt16LE(0);
+            fc.global_position_int.vy = Buffer.from(vy, 'hex').readInt16LE(0);
+            fc.global_position_int.vz = Buffer.from(vz, 'hex').readInt16LE(0);
+            fc.global_position_int.hdg = Buffer.from(hdg, 'hex').readUInt16LE(0);
+        }
+    } catch
+        (e) {
+        console.log('[parseMavFromDrone Error]', e);
+    }
+}
+
+function setSortieName(message){
+    let arr_message = message.toString().split(':');
+    my_sortie_name = arr_message[0];
+    let time_boot_ms = arr_message[1];
+
+    if (my_sortie_name === 'unknown-arm') { // 시작될 때 이미 드론이 시동이 걸린 상태
+        // 모비우스 조회해서 현재 sortie를 찾아서 설정함
+        let path = 'http://' + conf.cse.host + ':' + conf.cse.port + '/Mobius/' + conf.drone_info.gcs + '/Drone_Data/' + conf.drone_info.drone;
+        let cra = moment().utc().format('YYYYMMDD');
+
+        onem2m_client.getSortieLatest(path, cra, (status, uril) => {
+            if (uril.length === 0) {
+                // 현재 시동이 걸린 상태인데 오늘 생성된 sortie가 없다는 뜻이므로 새로 만듦
+                my_sortie_name = moment().format('YYYY_MM_DD_T_HH_mm');
+                prev_sortie_name = my_sortie_name;
+                my_cnt_name = my_parent_cnt_name + '/' + my_sortie_name;
+                onem2m_client.createSortieContainer(my_parent_cnt_name + '?rcn=0', my_sortie_name, time_boot_ms, 0, function (rsc, res_body, count) {
+                });
+                my_gimbal_name = my_gimbal_parent + '/' + my_sortie_name;
+                onem2m_client.crtct(my_gimbal_parent + '?rcn=0', my_sortie_name, 0, function (rsc, res_body, count) {
+                });
+            } else {
+                my_sortie_name = uril[0].split('/')[4];
+                prev_sortie_name = my_sortie_name;
+                my_cnt_name = my_parent_cnt_name + '/' + my_sortie_name;
+            }
+        });
+    } else if (my_sortie_name === 'unknown-disarm') { // 시작될 때 드론이 시동이 꺼진 상태
+        // disarm sortie 적용
+        my_sortie_name = 'disarm';
+        my_cnt_name = my_parent_cnt_name + '/' + my_sortie_name;
+    } else if (my_sortie_name === 'disarm-arm') { // 드론이 꺼진 상태에서 시동이 걸리는 상태
+        // 새로운 sortie 만들어 생성하고 설정
+        my_sortie_name = moment().format('YYYY_MM_DD_T_HH_mm');
+        prev_sortie_name = my_sortie_name;
+        my_cnt_name = my_parent_cnt_name + '/' + my_sortie_name;
+        onem2m_client.createSortieContainer(my_parent_cnt_name + '?rcn=0', my_sortie_name, time_boot_ms, 0, function (rsc, res_body, count) {
+        });
+        my_gimbal_name = my_gimbal_parent + '/' + my_sortie_name;
+        onem2m_client.crtct(my_gimbal_parent + '?rcn=0', my_sortie_name, 0, function (rsc, res_body, count) {
+        });
+    } else if (my_sortie_name === 'arm-disarm') { // 드론이 시동 걸린 상태에서 시동이 꺼지는 상태
+        // disarm sortie 적용
+        my_sortie_name = 'disarm';
+        my_cnt_name = my_parent_cnt_name + '/' + my_sortie_name;
+    }
+}
+/* */
+
